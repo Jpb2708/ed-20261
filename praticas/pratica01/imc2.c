502ff4aa41d3cf5f4c1bb1e278b10eccd1ba56e4
Joao Paulo <joaopaulobrandao2708@gmail.com>
2026-03-13 10:56:29 -0300

    adicionando aula 02

---
+++ praticas/pratica01/imc2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+float calcular_imc(float altura, float peso){
+if ( altura <= 0.0f || peso <= 0.0f) {
+    return 0.0f;
+}
+
+return peso/ (altura * altura);
+
+}
+int main (){
+    float imc = 0.00f;
+    imc = calcular_imc(0.0f,50.0f);
+    printf ("altura = 0, peso = 50, imc = %.1f => %i\n", imc , imc == 0);
+
+    imc = calcular_imc(1.75f, 0.0f);
+    printf("altura  = 1.75 , peso = 0, imc = %.1f => %i\n", imc , imc == 0);
+    
+    imc = calcular_imc(1.75f, 50.0f);
+    printf("altura  = 1.75 , peso = 50, imc = %.1f => %i\n", imc , imc < 18.5f);
+
+    imc = calcular_imc(1.75f, 65.0f);
+    printf("altura  = 1.75 , peso = 0, imc = %.1f => %i\n", imc , imc >=  18.5 && imc <= 24.9);
+
+    imc = calcular_imc(1.75f, 85.0f);
+    printf("altura  = 1.75 , peso = 0, imc = %.1f => %i\n", imc , imc =>  );
+    
+    imc = calcular_imc(1.75f, 95.0f);
+    printf("altura  = 1.75 , peso = 0, imc = %.1f => %i\n", imc , imc == 0);
+
+
+    return 0;
+}
